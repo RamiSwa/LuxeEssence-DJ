d7b604402a5b318c53b47b05cb18f57ff7750fa4
RamiSwa <118728140+RamiSwa@users.noreply.github.com>
2024-10-15 19:48:40 +0300

    Creating Admin Blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import BlogPost, Category
+
+
 
 # Register your models here.
+@admin.register(BlogPost)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publish', 'status', 'category')
+    list_filter = ('status', 'created', 'publish', 'author', 'category')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
+    show_facets = admin.ShowFacets.ALWAYS
